9d9f15a4e488f2dd16ba70f7baa54dc7af3e26d1
Matthew West <mrw324@lehigh.edu>
2023-05-18 15:35:41 -0700

    what the fuck

--- start/src/pages/Header/ConnectWallet.tsx
+++ start/src/pages/Header/ConnectWallet.tsx
@@ -1,10 +1,50 @@
 import React, { useEffect, useState } from "react";
+import { useWeb3React } from "@web3-react/core"
+//import { injected } from "./connectors"
 import './Header.css';
+import { InjectedConnector } from "@web3-react/injected-connector";
+
+const injected = new InjectedConnector({
+  supportedChainIds : [1,2,3,4]
+})
 
 const App: React.FC = () => {
 const [isMetamaskInstalled, setIsMetamaskInstalled] = useState<boolean>(false);
  const [ethereumAccount, setEthereumAccount] = useState<string | null>(null);
- 
+ /*const {active , account , library ,connector , activate , deactivate} = useWeb3React()
+
+  async function connect() {
+    try {
+      await activate(injected)
+      localStorage.setItem('isWalletConnected', String(true))
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate()
+      localStorage.setItem('isWalletConnected', String(false))
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  useEffect(() => {
+    const connectWalletOnPageLoad = async () => {
+      if (localStorage?.getItem('isWalletConnected') === 'true') {
+        try {
+          await activate(injected)
+          localStorage.setItem('isWalletConnected', String(true))
+        } catch (ex) {
+          console.log(ex)
+        }
+      }
+    }
+    connectWalletOnPageLoad()
+  }, [])*/
+
  useEffect(() => {
    if((window as any).ethereum){
      //check if Metamask wallet is installed
@@ -59,7 +99,14 @@ const [isMetamaskInstalled, setIsMetamaskInstalled] = useState<boolean>(false);
        </p>
        </div>
    </div>
- );
+ );/*
+ return (
+  <div className="flex flex-col items-center justify-center">
+    <button onClick={connect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect to MetaMask</button>
+    {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
+    <button onClick={disconnect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect</button>
+  </div>
+)*/
 }
  
 export default App;
